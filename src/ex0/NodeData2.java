3ed91f0eadd763c733e7147fd1282302bc3046ce
Eyal Levi <happyeyal@gmail.com>
2020-10-20 09:03:41 +0300

    replacing the arrayList with hashMap

---
+++ src/ex0/NodeData2.java
@@ -0,0 +1,55 @@
+package ex0;
+
+import java.util.Collection;
+
+public class NodeData2 implements node_data{
+
+
+    @Override
+    public int getKey() {
+        return 0;
+        return 0;
+        return 0;
+
+        return 0;
+    }
+
+    @Override
+    public Collection<node_data> getNi() {
+        return null;
+    }
+
+    @Override
+    public boolean hasNi(int key) {
+        return false;
+    }
+
+    @Override
+    public void addNi(node_data t) {
+
+    }
+
+    @Override
+    public void removeNode(node_data node) {
+    public void removeNode(node_data node) {
+    public void removeNode(node_data node) {
+    public String getInfo() {
+        return null;
+                    System.out.println()
+    }
+
+    @Override
+    public void setInfo(String s) {
+
+    }
+
+    @Override
+    public int getTag() {
+        return 0;
+    }
+
+    @Override
+    public void setTag(int t) {
+
+    }
+}
